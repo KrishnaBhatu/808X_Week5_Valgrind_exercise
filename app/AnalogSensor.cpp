642dd6f62832f514cb2fc1f79d90e3c7cb4248c1
KrishnaBhatu <kbhatu@terpmail.umd.edu>
2018-10-02 09:00:55 -0700

    Run valgrind and found 2 errors

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -15,7 +15,7 @@ int AnalogSensor::Read()
 {
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    double result = std::accumulate( readings->begin(), readings->end(), 0.0) / readings->size();
     return result;
 }
 
